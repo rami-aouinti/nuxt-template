569f1316080e98e223839f6994eff7e56851239b
unknown <rami.aouinti@gmail.com>
2025-11-06 01:44:21 +0100

    Nuxt Installation

---
+++ app/pages/blog.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+  icon: 'mdi-home',
+  title: 'Blog',
+  drawerIndex: 1,
+})
+</script>
+
+<template>
+  <v-container
+    fluid
+    class="d-flex align-items-center justify-center fill-height"
+  >
+  </v-container>
+</template>
---
+++ app/pages/nested.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Nested Routes',
+  icon: 'mdi-view-list',
+  drawerIndex: 3,
+})
+</script>
+<template>
+  <NuxtPage />
+</template>
