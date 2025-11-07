fbe0e0488a9846f9c91e1581a7f5df8100bfbd61
Rami <rami.aouinti@gmail.com>
2025-11-07 01:21:47 +0100

    Fix route loader visibility on navigation

--- app/plugins/route-loading.client.ts
+++ app/plugins/route-loading.client.ts
@@ -1,6 +1,18 @@
 export default defineNuxtPlugin((nuxtApp) => {
   const loading = useRouteLoading()
 
+  nuxtApp.hook('page:start', () => {
+    loading.value = true
+  })
+
+  nuxtApp.hook('page:finish', () => {
+    loading.value = false
+  })
+
+  nuxtApp.hook('page:error', () => {
+    loading.value = false
+  })
+
   nuxtApp.hook('page:loading:start', () => {
     loading.value = true
   })
