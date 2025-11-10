c1237c6a39dda9ca727feb188f586ffdd29b5e6c
Rami <rami.aouinti@gmail.com>
2025-11-10 23:39:12 +0100

    Replace flag icons with emoji-based flags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,7 +58,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/test-utils/module',
   ],
-  css: ['~/assets/styles/index.css', 'flag-icons/css/flag-icons.min.css'],
+  css: ['~/assets/styles/index.css'],
   experimental: { typedPages: true },
   typescript: {
     shim: false,
