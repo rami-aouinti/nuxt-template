40cfcab2092beb384aa1f20704c22bbb58a90723
rami aouinti <rami.aouinti@gmail.com>
2025-11-13 00:35:17 +0100

    add language

--- server/api/auth/verify.post.ts
+++ server/api/auth/verify.post.ts
@@ -1,3 +1,4 @@
+import { axios, AxiosError } from '~/utils/axios'
 import { defineEventHandler, readBody } from 'h3'
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
