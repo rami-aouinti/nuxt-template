75c3ae508cbcaf00a015c16838c6d5ff8f0a656c
Rami <rami.aouinti@gmail.com>
2025-11-14 05:07:39 +0100

    feat: add administrator type definitions

---
+++ app/types/administrator.ts
@@ -0,0 +1,113 @@
+export interface AdministratorOAuthAccount {
+  id?: number
+  provider: string
+  identifier: string
+  accessToken?: string | null
+  refreshToken?: string | null
+  user?: string | null
+}
+
+export interface AdministratorImageResource {
+  type?: string | null
+  file?: string | null
+  path?: string | null
+  owner?: unknown
+  id?: unknown
+}
+
+export type AdministratorImage = AdministratorImageResource | null
+
+export type HydraContext =
+  | string
+  | {
+      '@vocab'?: string
+      hydra?: 'http://www.w3.org/ns/hydra/core#'
+      [key: string]: unknown
+    }
+
+export interface Administrator {
+  firstName: string | null
+  lastName: string | null
+  localeCode: string
+  avatar?: string | null
+  id?: number
+  username: string | null
+  usernameCanonical?: string | null
+  password?: string | null
+  plainPassword?: string | null
+  lastLogin?: string | null
+  emailVerificationToken?: string | null
+  passwordResetToken?: string | null
+  passwordRequestedAt?: string | null
+  verifiedAt?: string | null
+  roles?: string[]
+  oauthAccounts?: AdministratorOAuthAccount[]
+  email?: string | null
+  emailCanonical?: string | null
+  createdAt?: string
+  updatedAt?: string | null
+  enabled?: boolean
+  image?: AdministratorImage
+  userIdentifier?: string
+  role?: string[]
+  verified?: boolean
+  oAuthAccounts?: AdministratorOAuthAccount[]
+}
+
+export interface AdministratorListItem {
+  firstName: string | null
+  lastName: string | null
+  localeCode: string
+  avatar?: string | null
+  id?: number
+  username: string | null
+  lastLogin?: string | null
+  verifiedAt?: string | null
+  email?: string | null
+  createdAt: string
+  updatedAt?: string | null
+  enabled?: boolean
+}
+
+export interface AdministratorCreatePayload {
+  firstName?: string | null
+  lastName?: string | null
+  localeCode: string
+  username?: string | null
+  plainPassword?: string | null
+  email?: string | null
+  enabled?: boolean
+}
+
+export interface AdministratorUpdatePayload {
+  firstName?: string | null
+  lastName?: string | null
+  localeCode: string
+  username?: string | null
+  plainPassword?: string | null
+  email?: string | null
+  enabled?: boolean
+}
+
+export interface AdministratorResetPasswordPayload {
+  newPassword: string
+  confirmNewPassword?: string | null
+}
+
+export interface AdministratorRequestResetPasswordEmailPayload {
+  email: string
+}
+
+export interface AdministratorJsonLd extends Administrator {
+  '@context'?: HydraContext
+  '@id'?: string
+  '@type'?: string
+}
+
+export interface AdministratorListItemJsonLd extends AdministratorListItem {
+  '@context'?: HydraContext
+  '@id'?: string
+  '@type'?: string
+}
+
+export interface AdministratorDetailJsonLd extends AdministratorJsonLd {}
