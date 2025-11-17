37c6b2769dd240aedfeaf014a7deace7f6086d42
Rami <rami.aouinti@gmail.com>
2025-11-17 02:47:52 +0100

    feat(api): add job resume templates endpoint

---
+++ server/utils/broWorldJobApi.ts
@@ -0,0 +1,7 @@
+import { createBroWorldRequest } from './broWorldApi'
+
+const BASE_URL = 'https://job.bro-world.org'
+
+export const broWorldJobRequest = createBroWorldRequest(BASE_URL, undefined, {
+  resolveToken: () => null,
+})
