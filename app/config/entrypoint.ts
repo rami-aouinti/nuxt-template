a13fc9f998e2908f8894088463d2160c57c92b4b
rami aouinti <rami.aouinti@gmail.com>
2025-11-23 08:44:46 +0100

    add language

--- app/config/entrypoint.ts
+++ app/config/entrypoint.ts
@@ -1,2 +1,2 @@
-export const ENTRYPOINT = window.location.origin + '/api/'
+export const ENTRYPOINT = 'https://education.bro-worl.org' + '/api/'
 //export const ENTRYPOINT = process.env.APP_API_PLATFORM_URL;
