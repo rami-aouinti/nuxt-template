b4ad64abfb7620876b046511d447d0f5641bb523
Rami <rami.aouinti@gmail.com>
2025-11-24 12:55:20 +0100

    Fix education editor imports and resolve axios base URL

--- app/config/api.ts
+++ app/config/api.ts
@@ -3,11 +3,30 @@ import axios from 'axios'
 /**
  * @type {axios.AxiosInstance}
  */
-const instance = axios.create({
-  baseURL:
+const fallbackHost =
+  process.env.NUXT_PUBLIC_SITE_URL ||
+  process.env.NUXT_SITE_URL ||
+  'http://localhost:3000'
+
+const resolveBaseUrl = () => {
+  const rawBaseUrl =
     process.env.NUXT_PUBLIC_EDUCATION_API_BASE_URL ||
     process.env.NUXT_EDUCATION_API_BASE_URL ||
-    '/api/education',
+    '/api/education'
+
+  if (/^https?:\/\//i.test(rawBaseUrl)) {
+    return rawBaseUrl
+  }
+
+  if (typeof window !== 'undefined') {
+    return `${window.location.origin}${rawBaseUrl}`
+  }
+
+  return `${fallbackHost.replace(/\/+$/, '')}${rawBaseUrl}`
+}
+
+const instance = axios.create({
+  baseURL: resolveBaseUrl(),
   headers: {
     Accept: 'application/ld+json',
   },
