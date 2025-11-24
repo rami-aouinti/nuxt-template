f703f67c3a9eabe2a629a2cba87677fd370f5d6c
Rami <rami.aouinti@gmail.com>
2025-11-24 12:27:17 +0100

    Guard tinymce imports on server

--- app/components/education/Editor.js
+++ app/components/education/Editor.js
@@ -1,36 +1,43 @@
 import Editor from '@tinymce/tinymce-vue'
 
-import 'tinymce/tinymce'
-import 'tinymce/icons/default'
-import 'tinymce/themes/silver'
-import 'tinymce/plugins/advlist'
-import 'tinymce/plugins/anchor'
-import 'tinymce/plugins/autolink'
-import 'tinymce/plugins/autoresize'
-import 'tinymce/plugins/autosave'
-import 'tinymce/plugins/charmap'
-import 'tinymce/plugins/code'
-import 'tinymce/plugins/codesample'
-import 'tinymce/plugins/directionality'
-import 'tinymce/plugins/emoticons'
-import 'tinymce/plugins/fullscreen'
-import 'tinymce/plugins/help'
-import 'tinymce/plugins/image'
-import 'tinymce/plugins/importcss'
-import 'tinymce/plugins/insertdatetime'
-import 'tinymce/plugins/link'
-import 'tinymce/plugins/lists'
-import 'tinymce/plugins/media'
-import 'tinymce/plugins/nonbreaking'
-import 'tinymce/plugins/pagebreak'
-import 'tinymce/plugins/preview'
-import 'tinymce/plugins/quickbars'
-import 'tinymce/plugins/save'
-import 'tinymce/plugins/searchreplace'
-import 'tinymce/plugins/table'
-import 'tinymce/plugins/visualblocks'
-import 'tinymce/plugins/visualchars'
-import 'tinymce/plugins/wordcount'
+const clientOnlyImports = [
+  'tinymce/tinymce',
+  'tinymce/icons/default',
+  'tinymce/themes/silver',
+  'tinymce/plugins/advlist',
+  'tinymce/plugins/anchor',
+  'tinymce/plugins/autolink',
+  'tinymce/plugins/autoresize',
+  'tinymce/plugins/autosave',
+  'tinymce/plugins/charmap',
+  'tinymce/plugins/code',
+  'tinymce/plugins/codesample',
+  'tinymce/plugins/directionality',
+  'tinymce/plugins/emoticons',
+  'tinymce/plugins/fullscreen',
+  'tinymce/plugins/help',
+  'tinymce/plugins/image',
+  'tinymce/plugins/importcss',
+  'tinymce/plugins/insertdatetime',
+  'tinymce/plugins/link',
+  'tinymce/plugins/lists',
+  'tinymce/plugins/media',
+  'tinymce/plugins/nonbreaking',
+  'tinymce/plugins/pagebreak',
+  'tinymce/plugins/preview',
+  'tinymce/plugins/quickbars',
+  'tinymce/plugins/save',
+  'tinymce/plugins/searchreplace',
+  'tinymce/plugins/table',
+  'tinymce/plugins/visualblocks',
+  'tinymce/plugins/visualchars',
+  'tinymce/plugins/wordcount',
+]
+
+if (typeof window !== 'undefined') {
+  await Promise.all(clientOnlyImports.map((path) => import(path)))
+}
 
 export { Editor }
 export default Editor
+
